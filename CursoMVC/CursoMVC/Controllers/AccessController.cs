5089c846afc543159ae9bf0c8b84b5e4bcb198a3
SoyBeckyOK <rebecasarai_gb@hotmail.com>
2019-07-05 09:55:14 -0600

    Video 2 terminado con exito

--- CursoMVC/CursoMVC/Controllers/AccessController.cs
+++ CursoMVC/CursoMVC/Controllers/AccessController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CursoMVC.Models;
 
 namespace CursoMVC.Controllers
 {
@@ -18,7 +19,24 @@ namespace CursoMVC.Controllers
         {
             try
             {
-                return Content("1");
+                using (db_cursomvcEntities db = new db_cursomvcEntities())
+                {
+                    var list = from d in db.tbl_usuarios
+                               where d.email == user && d.password == pass && d.idCatalogo == 1
+                               select d;
+                    if (list.Count()>0)
+                    {
+                        tbl_usuarios oUser = list.First();
+                        Session["User"] = oUser;
+                        return Content("1");
+                    }
+                    else
+                    {
+                        return Content("Usuario invalido....");
+                    }
+                }
+
+                
             }
             catch (Exception ex)
             {
